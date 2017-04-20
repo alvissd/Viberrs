05394616687ece0e8148e6a6b2948bbdc75eeccb
alvissd <alvissd0201@gmail.com>
2017-04-21 00:43:29 +0700

    lesson 33

--- music/urls.py
+++ music/urls.py
@@ -7,10 +7,12 @@ app_name = 'music'
 urlpatterns = [
     # /
     url(r'^$', views.IndexView.as_view(),name="index"),
-
     # /something/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(),name="detail"),
-
-    # /music/album/add/
+    #/music/album/add/
     url(r'album/add/$', views.AlbumCreate.as_view(),name="album_add"),
+    # /music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(),name="album_update"),
+    # /music/album/2/delete
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(),name="album_delete"),
 ]
